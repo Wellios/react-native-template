1349e395ad1947b4206607edb41f4593e242921e
Wellington Leite <wellingtonleitee.s@gmail.com>
2019-04-16 10:24:45 -0300

    Remoção de arquivos para habilitar uso de arquivo .jsx

--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-
 import { View } from 'react-native';
+import { Picker } from 'native-base'
 
 // import { Container } from './styles';
 
 export class HomeScreen extends Component {
   render() {
     return (
-      <View/>
+      <View />
     )
   }
 }
